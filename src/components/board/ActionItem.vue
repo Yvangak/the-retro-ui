cddf40fd8bfc179b88b73be41d69fc3bf49221d2
Yvan GAKUBA <gakyvan@gmail.com>
2020-04-23 15:34:10 -0400

    added ActionItem component to list the action items

---
+++ src/components/board/ActionItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="ui card">
+            <div class="content">
+                <div class="center aligned header">Action Item # 1</div>
+                <div class="center aligned description">
+                    <p>Jenny is a student studying Media Management at the New School</p>
+                </div>
+            </div>
+            <div class="extra content center aligned">
+                <div class="ui buttons">
+                    <button class="ui positive button"><i class="check icon"></i></button>
+                    <div class="or"></div>
+                    <button class="ui negative button"><i class="trash icon"></i></button>
+                </div>
+            </div>
+            <div class="extra content">
+                <div class="center aligned author">
+                    <img class="ui avatar image" src="https://semantic-ui.com/images/avatar/small/jenny.jpg"> Jenny
+                    <!--<div class="ui basic button simple dropdown item">
+                       <i class="user icon"></i> Choose responsible
+                       <div class="menu">
+                           <div class="item"><i class="chevron right icon"></i> Ygakuba</div>
+                           <div class="item"><i class="chevron right icon"></i> rmoussa</div>
+                           <div class="item"><i class="chevron right icon"></i> rdorn4</div>
+                       </div>
+                   </div>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/board/ActionItems.vue
+++ src/components/board/ActionItems.vue
@@ -4,12 +4,39 @@
             <span class="ui blue ribbon label"><i class="book icon" style="font-size: 14pt"></i> </span>
             Action Items
         </div>
-        <div class="ui fluid icon input">
-            <input type="text" placeholder="Action Item goes here...">
+        <div class="ui fluid icon input" :class="{error: isValid}">
+            <input type="text" placeholder="Action Item goes here..."
+                   @keyup.enter="saveActionItem"
+                   v-model="actionDescription">
             <i class="pencil alternate icon"></i>
         </div>
         <h4 class="ui horizontal divider header">
             <i class="minus icon"></i>
         </h4>
+        <app-action-item></app-action-item>
     </div>
 </template>
+<script>
+    import ActionItem from './ActionItem';
+
+    export default {
+        data() {
+            return {
+                actionDescription: '',
+                isValid: false,
+            };
+        },
+        components: {
+            appActionItem: ActionItem,
+        },
+        methods: {
+            saveActionItem(){
+                if(this.actionDescription.trim()!==''){
+                    this.isValid = false;
+                }else{
+                    this.isValid = true;
+                }
+            }
+        }
+    };
+</script>
